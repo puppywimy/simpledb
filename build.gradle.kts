303535cc88870f4cc945107f32a7e2d29b064790
puppywimy <eric250@naver.com>
2026-04-27 21:35:23 +0900

    chore: correct dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("java")
 }
 
-group = "com.ll"
+group = "com.back"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,19 +10,10 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.projectlombok:lombok:1.18.42")
-    annotationProcessor("org.projectlombok:lombok:1.18.42")
-
-    runtimeOnly("com.mysql:mysql-connector-j:9.5.0")
-
-    testImplementation("org.assertj:assertj-core:3.27.6")
-
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.20.1")
-
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.assertj:assertj-core:3.27.7")
 }
 
 tasks.test {
